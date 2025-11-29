fc1eb28afc3cba19503c28a12e74645ba2941b4f
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-29 23:08:44 +0530

    HelloWorld using Servlet with Tomcat

---
+++ Servlet/HelloWorld/HelloWorld/src/main/java/com/hello/helloworld.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello Servlet Working Successfully!</h1>");
+    }
+}
