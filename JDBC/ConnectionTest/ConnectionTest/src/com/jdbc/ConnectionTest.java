3aa9a095671ea9e0ab6b63b93d68d405db839af9
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-30 18:53:54 +0530

    establishing connection and inserting values

---
+++ JDBC/ConnectionTest/ConnectionTest/src/com/jdbc/ConnectionTest.java
@@ -0,0 +1,23 @@
+package com.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ConnectionTest {
+	public static void main(String args[]) {
+		String url="jdbc:mysql://localhost:3306/jdbc";
+		String user="root";
+		String pass="2002";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");//load driver
+			Connection con= DriverManager.getConnection(url, user, pass);//connection establish
+			Statement st=con.createStatement();
+			st.executeUpdate("INSERT INTO student(id, name, cgpa) VALUES(101, 'vamsi', 8.5)");
+			System.out.println("Data Inserted Succcessfully");
+			con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
